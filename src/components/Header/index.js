b1875f5d9a6893071c2c434968d861799c6ba183
praveen6289 <kuthadipraveen366@gmail.comgit config --global user.name praveen6289>
2022-06-29 09:21:45 +0000

    updated my projects app with logo's

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,19 +3,19 @@ import './index.css'
 const Header = () => (
   <nav className="nav-header">
     <div className="nav-content">
-      <p className="website-logo">RA</p>
+      <p className="website-logo">KP</p>
       <ul className="nav-menu">
         <li>
           <img
             className="social-network-img"
-            src="https://image.flaticon.com/icons/png/512/174/174857.png"
+            src="https://cdn-icons-png.flaticon.com/512/174/174857.png?w=740"
             alt="Linked In"
           />
         </li>
         <li>
           <img
             className="social-network-img"
-            src="https://image.flaticon.com/icons/png/512/25/25231.png"
+            src="https://cdn-icons-png.flaticon.com/512/38/38401.png?w=740"
             alt="Git Hub"
           />
         </li>
